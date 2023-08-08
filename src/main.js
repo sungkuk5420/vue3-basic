23bbf981c75a3a1155c45d8b0d4b9b2868e112f5
Sungkuk Kim <sungkuk5420@gmail.com>
2023-08-08 14:28:50 +0900

    Vue3 플러그인 만들기

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
-createApp(App).use(router).mount('#app');
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
+
+createApp(App)
+	.use(funcPlugins)
+	.use(person, { name: '홍길동' })
+	.use(objPlugins, { name: '짐코딩' })
+	.use(router)
+	.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
 
-console.log('MODE:', import.meta.env.MODE);
-console.log('BASE_URL:', import.meta.env.BASE_URL);
-console.log('PROD:', import.meta.env.PROD);
-console.log('DEV:', import.meta.env.DEV);
-console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL);
+// console.log('MODE:', import.meta.env.MODE);
+// console.log('BASE_URL:', import.meta.env.BASE_URL);
+// console.log('PROD:', import.meta.env.PROD);
+// console.log('DEV:', import.meta.env.DEV);
+// console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL);
---
+++ src/plugins/obj.js
@@ -0,0 +1,9 @@
+const objPlugins = {
+	install(app, options) {
+		console.log(app);
+		console.log(options);
+		console.log('objPlugins');
+	},
+};
+
+export default objPlugins;
